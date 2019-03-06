ff2c79ab296dcf76de8d694d87d658492c0a8a90
Petrov Vladimir <neoman36@gmail.com>
2019-03-06 12:42:33 +0300

    Make pair url+title unique

--- api/models.py
+++ api/models.py
@@ -5,3 +5,6 @@ class News(models.Model):
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('title', 'url')
