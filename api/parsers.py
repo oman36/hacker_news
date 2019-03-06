7c144b4b3385019093b0d81b85e3204e3e86247e
Petrov Vladimir <neoman36@gmail.com>
2019-03-06 12:38:50 +0300

    Add test for HackerNewsHtmlParser

--- api/parsers.py
+++ api/parsers.py
@@ -7,38 +7,39 @@ class HackerNewsHtmlParser(HTMLParser):
 
     def __init__(self, *, convert_charrefs=True):
         super().__init__(convert_charrefs=convert_charrefs)
-        self.in_table = False
-        self.in_athing = False
-        self.in_title = False
-        self.last_url = None
+        self._in_table = False
+        self._in_athing = False
+        self._in_title = False
+        self._last_url = None
         self.pairs = []
 
     def handle_starttag(self, tag, attrs):
-        if self.in_title and 'a' == tag and ('class', 'storylink') in attrs:
+        if self._in_title and 'a' == tag and ('class', 'storylink') in attrs:
             for attr, val in attrs:
                 if attr == 'href':
-                    self.last_url = val
+                    self._last_url = val.strip()
                     break
-        elif self.in_athing and 'td' == tag and ('class', 'title') in attrs:
-            self.in_title = True
-        elif self.in_table and 'tr' == tag and ('class', 'athing') in attrs:
-            self.in_athing = True
-        elif not self.in_table and 'table' == tag and ('class', 'itemlist') in attrs:
-            self.in_table = True
+        elif self._in_athing and 'td' == tag and ('class', 'title') in attrs:
+            self._in_title = True
+        elif self._in_table and 'tr' == tag and ('class', 'athing') in attrs:
+            self._in_athing = True
+        elif not self._in_table and 'table' == tag and ('class', 'itemlist') in attrs:
+            self._in_table = True
 
     def handle_endtag(self, tag):
-        if self.last_url is not None and 'a' == tag:
-            self.last_url = None
-        if self.in_title and 'td' == tag:
-            self.in_title = False
-        if self.in_athing and 'tr' == tag:
-            self.in_title = False
-        if self.in_table and 'table' == tag:
-            self.in_title = False
+        if self._last_url is not None and 'a' == tag:
+            self._last_url = None
+        if self._in_title and 'td' == tag:
+            self._in_title = False
+        if self._in_athing and 'tr' == tag:
+            self._in_athing = False
+        if self._in_table and 'table' == tag:
+            self._in_table = False
 
     def handle_data(self, data):
-        if self.last_url is not None:
-            self.pairs.append((self.last_url, data.strip()))
+        if self._last_url is not None:
+            self.pairs.append((self._last_url, data.strip()))
+            self._last_url = None
 
 
 class HackerNewsHtmlParserException(Exception):
---
+++ api/tests/parsers.py
@@ -0,0 +1,115 @@
+from django.test import TestCase
+from api.parsers import HackerNewsHtmlParser
+
+
+class HackerNewsHtmlParserTestCase(TestCase):
+    def test_pairs(self):
+        parser = HackerNewsHtmlParser()
+        parser.feed("""\
+        <table class="itemlist">
+            <tr class="athing">
+                <td class="title">
+                    <a href="url1" class="storylink">
+                        title1
+                    </a>
+                </td>
+            </tr>
+            <tr class="athing">
+                <td class="title">
+                    <a href="url2" class="storylink">
+                        title2
+                    </a>
+                </td>
+            </tr>
+        </table>""")
+        self.assertEqual(parser.pairs, [('url1', 'title1'), ('url2', 'title2')])
+
+    def test_pairs_fails(self):
+        cases = {
+            "Table class not checked": """
+                <table class="table_class">
+                    <tr class="athing">
+                        <td class="title">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </td>
+                    </tr>
+                </table>""",
+            "Row class not checked": """
+                <table class="itemlist">
+                    <tr class="other_row_class">
+                        <td class="title">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </td>
+                    </tr>
+                </table>""",
+            "Cell class not checked": """
+                <table class="itemlist">
+                    <tr class="athing">
+                        <td class="cell_class">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </td>
+                    </tr>
+                </table>""",
+            "Link class not checked": """
+                <table class="itemlist">
+                    <tr class="athing">
+                        <td class="title">
+                            <a href="url1" class="link_class">
+                                title1
+                            </a>
+                        </td>
+                    </tr>
+                </table>""",
+
+            # tags
+            "Table tag not checked": """
+                <ul class="itemlist">
+                    <tr class="athing">
+                        <td class="title">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </td>
+                    </tr>
+                </ul>""",
+            "Row tag not checked": """
+                <table class="itemlist">
+                    <li class="athing">
+                        <td class="title">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </td>
+                    </li>
+                </table>""",
+            "Cell tag not checked": """
+                <table class="itemlist">
+                    <tr class="athing">
+                        <span class="title">
+                            <a href="url1" class="storylink">
+                                title1
+                            </a>
+                        </span>
+                    </tr>
+                </table>""",
+            "Link tag not checked": """
+                <table class="itemlist">
+                    <tr class="athing">
+                        <td class="title">
+                            <div href="url1" class="storylink">
+                                title1
+                            </div>
+                        </td>
+                    </tr>
+                </table>""",
+        }
+        for msg, case in cases.items():
+            parser = HackerNewsHtmlParser()
+            parser.feed(case)
+            self.assertEqual([], parser.pairs, msg)
