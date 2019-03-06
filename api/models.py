4e743a7db10956daed418cbf6dc52c16ec434fa5
Petrov Vladimir <neoman36@gmail.com>
2019-03-06 10:41:53 +0300

    Add model news

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
